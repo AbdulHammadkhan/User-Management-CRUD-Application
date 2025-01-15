bc1022b073191ad4d4d04f99919c5f586fcfa97d
Abdul_Hammad_Khan <156004379+AbdulHammadkhan@users.noreply.github.com>
2025-01-15 15:13:09 -0400

    Add files via upload
    This project is a CRUD application built with Spring Boot(Java) for the backend and React for the frontend. It allows users to be registered, edited, deleted, and viewed seamlessly.
    
    Features:
    Register Users: Add new user details. 
    Edit Details: Update existing user information.
    Delete Users: Remove users from the database. 
    View Users: Display all registered users in a user-friendly interface.
    
    Technologies:
    Backend: Spring Boot, Spring Data JPA, REST APIs
    Frontend: React, React Hooks
    Database: MySQL
    This application showcases smooth integration between the backend and frontend for efficient user management.
    
    Prerequisites Before you begin, ensure you have met the following requirements:
    Java Development Kit (JDK) installed. Node.js and npm installed. IDE with support for Spring Boot and React development

---
+++ CRUD_Backend/src/main/java/com/codes/fullstackbackend/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.codes.fullstackbackend.repository;
+
+import com.codes.fullstackbackend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+}
